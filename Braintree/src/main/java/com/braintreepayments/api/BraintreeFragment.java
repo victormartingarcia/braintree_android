aa6aad1e0c9110fbf8b45f08451015167ae50fa6
quinnjn <quinn@neumiiller.com>
2018-10-18 16:32:35 -0700

    fixup! Synchronize the callback queue while we flush

--- Braintree/src/main/java/com/braintreepayments/api/BraintreeFragment.java
+++ Braintree/src/main/java/com/braintreepayments/api/BraintreeFragment.java
@@ -736,7 +736,8 @@ public class BraintreeFragment extends BrowserSwitchFragment {
     @VisibleForTesting
     protected void flushCallbacks() {
         synchronized (mCallbackQueue) {
-            for (QueuedCallback callback : mCallbackQueue) {
+            Queue<QueuedCallback> queue = new ArrayDeque<>(mCallbackQueue);
+            for (QueuedCallback callback : queue) {
                 if (callback.shouldRun()) {
                     callback.run();
                     mCallbackQueue.remove(callback);
